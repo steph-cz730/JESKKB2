ee981dec624cdd1246f1518aa71ad7d85e995cc9
kenharris83 <kenn.harris83@gmail.com>
2024-01-20 15:07:27 -0500

    Completed backend Activity

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
-const cors = require('cors')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const users = require('./routes/users')
 
