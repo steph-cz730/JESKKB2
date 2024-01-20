a0bd199bba109f579b18021a3abf2d2f58ae8a0b
Kristen Scheffler <kris10ben10@gmail.com>
2024-01-20 16:20:31 -0500

    few adjustments

---
+++ frontend/src/Components/About.js
@@ -0,0 +1,26 @@
+// src/About.js
+import React from "react";
+
+const About = () => {
+  return (
+    <section>
+      <h2>About Us</h2>
+      <div className="team-member">
+        {/* <img src="professional pic1" alt="Team Member 1" /> */}
+        <div>
+          <h3>Team Member 1</h3>
+          {/* <p>Info for member 1</p> */}
+        </div>
+      </div>
+      <div className="team-member">
+        {/* <img src="professional pic2" alt="Team Member 2" /> */}
+        <div>
+          <h3>Team Member 2</h3>
+          {/* <p>Info for member 2</p> */}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
